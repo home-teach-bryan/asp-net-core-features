898e09e366d1dd767b2f0ee18ad3e9c59f6b371f
d58526 <d58526@gmail.com>
2024-09-09 22:16:20 +0800

    update yaml and code

--- AspNetCoreSampleFeature/AspNetCoreFeature/Services/UserService.cs
+++ AspNetCoreSampleFeature/AspNetCoreFeature/Services/UserService.cs
@@ -9,6 +9,7 @@ public class UserService : IUserService
         new User { Id = Guid.NewGuid(), Name = "user1", Password = "123456", Roles = new[] { "Admin", "User" } },
         new User { Id = Guid.NewGuid(), Name = "user2", Password = "456789", Roles = new[] { "User" } },
         new User { Id = Guid.NewGuid(), Name = "user3", Password = "aa123456", Roles = new[] { "User" } },
+        new User { Id = Guid.NewGuid(), Name = "user4", Password = "aabbcc", Roles = new[] { "User" } },
     };
 
     public (bool isValid, User user) IsValid(string name, string password)
